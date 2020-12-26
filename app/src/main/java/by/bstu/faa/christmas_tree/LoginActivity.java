bc0dc959f161fd4a7b34e9656b8b8a9cb4dab78c
DOJ745 <andreyfa12@mail.ru>
2020-12-26 20:03:35 +0300

    Work with login

--- app/src/main/java/by/bstu/faa/christmas_tree/LoginActivity.java
+++ app/src/main/java/by/bstu/faa/christmas_tree/LoginActivity.java
@@ -23,6 +23,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        initViews();
     }
 
     public void onStart() {
