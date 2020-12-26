9d61343096db246f4ac54a0330e44d90f0811003
DOJ745 <andreyfa12@mail.ru>
2020-12-26 23:09:36 +0300

    show dialog works, I guess

--- app/src/main/java/by/bstu/faa/christmas_tree/MainActivity.java
+++ app/src/main/java/by/bstu/faa/christmas_tree/MainActivity.java
@@ -1,39 +1,49 @@
 package by.bstu.faa.christmas_tree;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import by.bstu.faa.christmas_tree.custom_dialog.MyDialogFragment;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static String user_name = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showDialog();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FragmentManager manager = getSupportFragmentManager();
-        MyDialogFragment myDialogFragment = new MyDialogFragment();
-        myDialogFragment.show(manager, "myDialog");
-    }
-
-    public void onClick(View view) {
         /*FragmentManager manager = getSupportFragmentManager();
         MyDialogFragment myDialogFragment = new MyDialogFragment();
         myDialogFragment.show(manager, "myDialog");*/
     }
 
+    private void showDialog() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog, null);
+        EditText entered_name = view.findViewById(R.id.edit_name);
+
+        myDialog.setTitle("Ho-ho-ho");
+        myDialog.setIcon(R.drawable.christmas_bauble_32);
+        myDialog.setView(view);
+
+        myDialog.setPositiveButton("OK", (dialog, which) -> {
+            // TODO Auto-generated method stub
+            user_name = entered_name.getText().toString();
+
+            Toast.makeText(MainActivity.this, "Your name is " + user_name,  Toast.LENGTH_SHORT).show();
+        });
+        myDialog.show();
+    }
 }
