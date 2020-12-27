cdade655551df741936ba71720e157c5b2b8a63e
DOJ745 <andreyfa12@mail.ru>
2020-12-27 13:42:46 +0300

    Added pictures and simple methods to UserInfo

--- app/src/main/java/by/bstu/faa/christmas_tree/model/UserInfo.java
+++ app/src/main/java/by/bstu/faa/christmas_tree/model/UserInfo.java
@@ -15,4 +15,17 @@ public class UserInfo implements Serializable {
     private String name;
     private int treeLevel;
     private int score;
+
+    public void addLevel() { this.treeLevel += 1; }
+    public void reduceLevel() {
+        if(this.treeLevel - 1 < 0) { this.treeLevel = 0; }
+        else this.treeLevel -= 1;
+    }
+
+    public void addPoints() { this.score += 10; }
+
+    public void reducePoints() {
+        if(this.score - 5 < 0) { this.score = 0; }
+        else this.score -= 5;
+    }
 }
